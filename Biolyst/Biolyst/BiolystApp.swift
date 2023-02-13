2a38bf85a636c6f98e60aacde0001d70d818c547
Cristian Vaccarella <102797129+TheCris94@users.noreply.github.com>
2023-02-13 15:22:11 +0100

    Add files via upload

---
+++ Biolyst/Biolyst/BiolystApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiolystApp.swift
+//  Biolyst
+//
+//  Created by cristianvaccarella on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct BiolystApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Biolyst/Biolyst/MainView.swift
@@ -0,0 +1,26 @@
+//
+//  MainView.swift
+//  Biolyst
+//
+//  Created by cristianvaccarella on 13/02/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack{ //stack dell'immagine di copertina
+            Image("mainImage").resizable().frame(width: 393, height: 200).ignoresSafeArea()
+                .overlay(HStack{
+                    Text("BIOLYST").font(.system(size: 30)).fontWeight(.bold).foregroundColor(.white)
+                }.position(x:110, y:75))
+            Spacer()
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
