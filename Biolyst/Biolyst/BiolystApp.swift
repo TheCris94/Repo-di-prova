4210675845f690c7937fb92274a4572d151dadb8
smog84 <125269227+smog84@users.noreply.github.com>
2023-02-15 18:10:19 +0100

    Biolyst updated

--- Biolyst/Biolyst/BiolystApp.swift
+++ Biolyst/Biolyst/BiolystApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct BiolystApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            //ContentView()
+            MainView()
         }
     }
 }
--- Biolyst/Biolyst/MainView.swift
+++ Biolyst/Biolyst/MainView.swift
@@ -8,19 +8,37 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var selectedTab = "All"
+    var tabs = ["All", "Ongoing"]
+    
     var body: some View {
-        VStack{ //stack dell'immagine di copertina
-            Image("mainImage").resizable().frame(width: 393, height: 200).ignoresSafeArea()
-                .overlay(HStack{
-                    Text("BIOLYST").font(.system(size: 30)).fontWeight(.bold).foregroundColor(.white)
-                }.position(x:110, y:75))
-            Spacer()
-        }//mah
+        ZStack{
+            VStack{ //stack dell'immagine di copertina
+                Image("mainImage").resizable().frame(width: 393, height: 200).ignoresSafeArea().opacity(0.85)
+                    .overlay(HStack{
+                        Text("BIOLYST").font(.system(size: 30)).fontWeight(.bold).foregroundColor(.white)
+                    }.position(x:110, y:75))
+                Spacer()
+            }
+            Picker("What is your favorite color?", selection: $selectedTab){
+                ForEach(tabs, id: \.self) {
+                    Text($0)
+                }
+            }
+            .pickerStyle(.segmented)
+            .background (
+                    RoundedRectangle(cornerRadius:8)
+                    .fill (Color.accentColor)
+                                )
+            
+            .position(x:200, y:120)
+            
+        }
     }
-}
-
-struct MainView_Previews: PreviewProvider {
-    static var previews: some View {
-        MainView()
+    
+    struct MainView_Previews: PreviewProvider {
+        static var previews: some View {
+            MainView()
+        }
     }
 }
